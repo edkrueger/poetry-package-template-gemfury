72426a583486530eae1e744730a4a1361330b6dc
edkrueger <edkrueger@gmail.com>
2021-01-08 11:13:59 -0600

    Finish configuring tests.

---
+++ resc/demo.py
@@ -0,0 +1,13 @@
+"""
+Some math functions.
+"""
+
+
+def add(a_num, b_num):
+    """Adds two numbers."""
+    return a_num + b_num
+
+
+def sub(a_num, b_num):
+    """Subtract two numbers."""
+    return a_num - b_num
