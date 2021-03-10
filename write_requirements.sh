ac6053a1a064379469daeadac0958bd7503f74cd
edkrueger <edkrueger@gmail.com>
2021-03-10 11:49:06 -0600

    Write requirements-dev.txt with hook.

--- write_requirements.sh
+++ write_requirements.sh
@@ -1,20 +1,48 @@
+FAILED=false
+
+# requirements
 NEW_REQUIREMENTS=$(poetry export -f requirements.txt --without-hashes)
 
 if [ -f requirements.txt ]; then
     echo "requirements.txt exists!"
 else
     echo "FAILURE: requirements.txt does not exist!"
-    poetry export -f requirements.txt --output requirements.txt --without-hashes
-    exit 1
+    poetry export --format requirements.txt --output requirements.txt --without-hashes
+    FAILED=True
 fi
 
 REQUIREMENTS=$(cat requirements.txt)
 
 if [ "$NEW_REQUIREMENTS" = "$REQUIREMENTS" ]; then
     echo "requirements.txt is up to date!"
-    exit 0
 else
     echo "FAILURE: requirements.txt is not up to date!"
-    poetry export -f requirements.txt --output requirements.txt --without-hashes
+    poetry export --format requirements.txt --output requirements.txt --without-hashes
+    FAILED=True
+fi
+
+# dev requirements
+NEW_REQUIREMENTS_DEV=$(poetry export --format requirements.txt --without-hashes --dev)
+
+if [ -f requirements-dev.txt ]; then
+    echo "requirements-dev.txt exists!"
+else
+    echo "FAILURE: requirements-dev.txt does not exist!"
+    poetry export --format requirements.txt --output requirements-dev.txt --without-hashes --dev
+    FAILED=True
+fi
+
+REQUIREMENTS_DEV=$(cat requirements-dev.txt)
+
+if [ "$NEW_REQUIREMENTS_DEV" = "$REQUIREMENTS_DEV" ]; then
+    echo "requirements-dev.txt is up to date!"
+else
+    echo "FAILURE: requirements-dev.txt is not up to date!"
+    poetry export --format requirements.txt --output requirements-dev.txt --without-hashes --dev
+    FAILED=true
+fi
+
+if [ "$FAILED" = true ]; then
     exit 1
 fi
+exit 0
